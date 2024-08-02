e33110e062f188cc45a982bd8c4f16ee4a4382c8
sunxiufei <xiufeifrank@gmail.com>
2024-08-02 22:49:59 +0800

    feat: 初始化工程

---
+++ gulimall-coupon/src/main/java/com/atgugui/gulimall/common/GulimallCouponApplication.java
@@ -0,0 +1,11 @@
+package com.atgugui.gulimall.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallCouponApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallCouponApplication.class, args);
+    }
+}
---
+++ gulimall-gateway/src/main/java/com/gulimall/ware/GulimallGatewayApplication.java
@@ -0,0 +1,11 @@
+package com.gulimall.ware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallGatewayApplication.class, args);
+    }
+}
